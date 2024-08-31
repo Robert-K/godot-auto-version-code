ae0c3925c98b9f48b214fed501015b25104d9d5d
Robert Kossessa <github@kosro.de>
2024-08-31 19:42:47 +0200

    FIX: Override version/code so changes don't require an editor restart ✨

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ It works by registering an [Export Plugin](https://docs.godotengine.org/en/stabl
 
 It's super simple, so feel free to modify it to suit your needs.
 
+## Installation
+
+Install it using the [Asset Library](https://godotengine.org/asset-library/asset/3286) or place the `auto_version_code` folder in your `addons` folder manually.
+
+## Tipp:
+
+This plugin works well in combination with the [Auto Export Version Plugin](https://godotengine.org/asset-library/asset/1430), which will allow you to get the version code in your game.
+For that you'll need to configure it to use the Android version code.
+
 [![forthebadge](https://forthebadge.com/images/featured/featured-uses-badges.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/featured/featured-made-with-crayons.svg)](https://forthebadge.com)
--- addons/auto_version_code/export_plugin.gd
+++ addons/auto_version_code/export_plugin.gd
@@ -4,24 +4,49 @@ extends EditorExportPlugin
 const EXPORT_PRESETS_PATH := "res://export_presets.cfg"
 const VERSION_CODE_KEY := "version/code"
 
-var increment_queued := false
+var version_code: int = 0
+var increment_queued: bool = false
+
+func _get_name():
+	return "Auto Version Code"
+
+func _supports_platform(platform: EditorExportPlatform) -> bool:
+	return true
+
+func _get_export_options_overrides(platform: EditorExportPlatform) -> Dictionary:
+	if not find_version_code(): return {}
+	print("Overriding ", VERSION_CODE_KEY,": ", version_code)
+	return {VERSION_CODE_KEY: version_code}
+
+func load_export_presets():
+	var export_presets = ConfigFile.new()
+	var error = export_presets.load(EXPORT_PRESETS_PATH)
+	if error != OK:
+		printerr("Error loading export presets at: ", EXPORT_PRESETS_PATH)
+		return null
+	return export_presets
+
+func find_version_code():
+	var export_presets = load_export_presets()
+	if export_presets == null: return false
+	for section in export_presets.get_sections():
+		if export_presets.has_section_key(section, VERSION_CODE_KEY):
+			version_code = int(export_presets.get_value(section, VERSION_CODE_KEY))
+			return true
+
+	printerr("No ", VERSION_CODE_KEY," found in export presets at: ", EXPORT_PRESETS_PATH)
+	return false
 
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int):
 	increment_queued = not is_debug
 
 func _export_end():
 	if increment_queued:
-		var export_presets = ConfigFile.new()
-		var error = export_presets.load(EXPORT_PRESETS_PATH)
-		if error != OK:
-			printerr("Error loading export presets at: ", EXPORT_PRESETS_PATH)
-			return
-
-		# Iterate over all sections.
+		version_code += 1
+		var export_presets = load_export_presets()
+		if export_presets == null: return
 		for section in export_presets.get_sections():
 			if export_presets.has_section_key(section, VERSION_CODE_KEY):
-				var version_code := export_presets.get_value(section, VERSION_CODE_KEY)
-				version_code = int(version_code) + 1
-				print("Incrementing [", section, "] ", VERSION_CODE_KEY, " to ", version_code)
+				print("Setting [", section, "] ", VERSION_CODE_KEY, " to ", version_code)
 				export_presets.set_value(section, VERSION_CODE_KEY, version_code)
-				export_presets.save(EXPORT_PRESETS_PATH)
+		export_presets.save(EXPORT_PRESETS_PATH)
